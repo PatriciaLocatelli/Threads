b8c866f19f708d9c2ee056dac042ebcbc1542f39
patriciaLocatelli <paty.lw502@gmail.com>
2020-02-04 14:14:24 -0300

    Testando as Threads

---
+++ src/threads/MinhaThread.java
@@ -0,0 +1,26 @@
+package threads;
+
+public class MinhaThread extends Thread {
+
+    private String nome;
+    private int tempo;
+
+    public MinhaThread(String nome, int tempo) {
+        this.nome = nome;
+        this.tempo = tempo;
+        start();
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i < 6; i++) {
+                System.out.println(nome + " contador " + i);
+                Thread.sleep(tempo);
+            }
+            System.out.println(nome + " terminou a execucao ");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
